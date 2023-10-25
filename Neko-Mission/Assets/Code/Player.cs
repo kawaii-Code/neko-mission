f3366404df6233b64c92d6688e02e5e43cd57e8b
Michael Sidorchenko <qternel@gmail.com>
2023-10-26 00:01:04 +0300

    Добавил увеличение баланса с течением времени и постройку башен за деньги
    Closes #9

--- Neko-Mission/Assets/Code/Player.cs
+++ Neko-Mission/Assets/Code/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -16,9 +17,11 @@ public class Player : MonoBehaviour
     private Rigidbody _rigidbody;
     private Transform _groundCheckObj;
     private bool _isGrounded;
-    private float _rotationX =0;
+    private float _rotationX = 0;
     private Camera _camera;
-    
+    public static long CurrentBalance = 0;
+    public int AddBalanceTime;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -26,33 +29,46 @@ public class Player : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        if (AddBalanceTime > 0)
+        {
+            InvokeRepeating("AddBalance", AddBalanceTime, AddBalanceTime);
+        }
+
     }
 
     void Update()
     {
         //Механика передвижения.
-        float deltaHor = Input.GetAxis("Horizontal")*MovingSpeed;
-        float deltaVer = Input.GetAxis("Vertical")*MovingSpeed;
-       Vector3 newpositionvec = transform.forward*deltaVer*MovingSpeed*Time.deltaTime;
-       Vector3 newpositionhor = transform.right* deltaHor*MovingSpeed*Time.deltaTime;
-        _rigidbody.MovePosition(transform.position+newpositionvec+newpositionhor);
+        float deltaHor = Input.GetAxis("Horizontal") * MovingSpeed;
+        float deltaVer = Input.GetAxis("Vertical") * MovingSpeed;
+        Vector3 newpositionvec = transform.forward * deltaVer * MovingSpeed * Time.deltaTime;
+        Vector3 newpositionhor = transform.right * deltaHor * MovingSpeed * Time.deltaTime;
+        _rigidbody.MovePosition(transform.position + newpositionvec + newpositionhor);
         //Механика прыжка.
-       _isGrounded = Physics.Raycast(_groundCheckObj.transform.position,Vector3.down,_maxGroundDist);
-       bool hasJump = Input.GetKeyDown(KeyCode.Space);
-       if (hasJump && _isGrounded)
-       {
+        _isGrounded = Physics.Raycast(_groundCheckObj.transform.position, Vector3.down, _maxGroundDist);
+        bool hasJump = Input.GetKeyDown(KeyCode.Space);
+        if (hasJump && _isGrounded)
+        {
             _rigidbody.AddForce(Vector3.up * 100 * JumpSpeed);
-       }
-       else if (!_isGrounded)
-       {
+        }
+        else if (!_isGrounded)
+        {
             _rigidbody.AddForce(Vector3.down * 100);
-       }
+        }
 
         //Вид от 1-го лица.
-        _rotationX -=Input.GetAxis("Mouse Y") * RotationSpeedVer;
-        _rotationX = Mathf.Clamp(_rotationX,minVer,maxVer);
+        _rotationX -= Input.GetAxis("Mouse Y") * RotationSpeedVer;
+        _rotationX = Mathf.Clamp(_rotationX, minVer, maxVer);
         float delta = Input.GetAxis("Mouse X") * RotationSpeedHor;
         float _rotationY = transform.localEulerAngles.y + delta;
-        transform.localEulerAngles = new Vector3(_rotationX,_rotationY,0);
+        transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
     }
+    //Добавление денег 
+    private void AddBalance()
+    {
+        CurrentBalance++;
+        Debug.Log($"CurrentBalance = {CurrentBalance}");
+    }
+
 }
--- Neko-Mission/Assets/Code/Spawner.cs
+++ Neko-Mission/Assets/Code/Spawner.cs
@@ -6,15 +6,16 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject Tower;
+    public int TowerPrice;
     private bool _towerIsBuilt = false;
 
     public void OnMouseDown()
     {
-        if (!_towerIsBuilt)
+        if (!_towerIsBuilt && Player.CurrentBalance >= TowerPrice)
         {
             Instantiate(Tower, transform.position + Vector3.up * Tower.transform.localScale.y / 2, Quaternion.identity);
+            Player.CurrentBalance -= TowerPrice;
             _towerIsBuilt = true;
         }
     }
-
 }
