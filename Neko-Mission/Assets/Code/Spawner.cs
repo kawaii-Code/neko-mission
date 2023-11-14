d309a4c0a5bf6eb4ca7c9e3fd486b4699761a0a5
Michael Sidorchenko <qternel@gmail.com>
2023-11-14 21:38:07 +0300

    Добавил добавление баланса со временем и построение башен за деньги
     Closes #9

--- Neko-Mission/Assets/Code/Spawner.cs
+++ Neko-Mission/Assets/Code/Spawner.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
     public GameObject Tower;
+<<<<<<< Updated upstream
+=======
+    public Player Pl;
+>>>>>>> Stashed changes
     public int TowerPrice;
     private bool _towerIsBuilt = false;
 
     public void OnMouseDown()
     {
+<<<<<<< Updated upstream
         if (!_towerIsBuilt && Player.CurrentBalance >= TowerPrice)
         {
             Instantiate(Tower, transform.position + Vector3.up * Tower.transform.localScale.y / 2, Quaternion.identity);
@@ -19,3 +23,13 @@ public class Spawner : MonoBehaviour
         }
     }
 }
+=======
+        if (!_towerIsBuilt && Pl.CurrentBalance >= TowerPrice)
+        {
+            Instantiate(Tower, transform.position + Vector3.up * Tower.transform.localScale.y / 2, Quaternion.identity);
+            Pl.CurrentBalance -= TowerPrice;
+            _towerIsBuilt = true;
+        }
+    }
+}
+>>>>>>> Stashed changes
