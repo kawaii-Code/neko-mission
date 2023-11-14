a05a2c277ecc8ad311c81135360cd36a5601c8f8
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-11-14 17:36:41 +0300

    Улучшил движение игрока (смотреть описание)
    * Раньше игрок поворачивался по оси X, из-за
    того что нужно было поворачивать камеру. В связи
    с этим игрок застревал в странных местах и иногда
    плохо работал. Я убрал это, теперь игрок поворачивается
    только по оси Y, а камера вращается отдельно.
    
    * Из-за того что я убрал вращение, выявился ещё один
    баг: прыжок не срабатывал, из-за того что GroundCheck
    выпирал за саму землю (сложно описать). Вместо этого
    я задвинул GroundCheck в самого игрока поглубже и добавил
    игрока в IgnoreRaycast, чтобы луч, проверяющий на земле
    ли игрок, не срабатывал от самого игрока.

--- Neko-Mission/Assets/Code/Player.cs
+++ Neko-Mission/Assets/Code/Player.cs
@@ -9,13 +9,16 @@ public class Player : MonoBehaviour
     public float MaxVer = 45.0f;
     public float MovingSpeed;
     public float JumpSpeed;
+    public LayerMask GroundMask;
+    
     private float _maxGroundDist = 0.6f;
     private Rigidbody _rigidbody;
     private Transform _groundCheckObj;
     private bool _isGrounded;
-    private float _rotationX =0;
+    private float _rotationX = 0;
+    private float _rotationY = 0;
     private Camera _camera;
-    
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -28,28 +31,30 @@ public class Player : MonoBehaviour
     void Update()
     {
         //Механика передвижения.
-        float deltaHor = Input.GetAxis("Horizontal")*MovingSpeed;
-        float deltaVer = Input.GetAxis("Vertical")*MovingSpeed;
-       Vector3 newpositionvec = transform.forward*deltaVer*MovingSpeed*Time.deltaTime;
-       Vector3 newpositionhor = transform.right* deltaHor*MovingSpeed*Time.deltaTime;
-        _rigidbody.MovePosition(transform.position+newpositionvec+newpositionhor);
+        float deltaHor = Input.GetAxis("Horizontal") * MovingSpeed;
+        float deltaVer = Input.GetAxis("Vertical") * MovingSpeed;
+        Vector3 newpositionvec = transform.forward * (deltaVer * MovingSpeed * Time.deltaTime);
+        Vector3 newpositionhor = transform.right * (deltaHor * MovingSpeed * Time.deltaTime);
+        _rigidbody.MovePosition(transform.position + newpositionvec + newpositionhor);
         //Механика прыжка.
-       _isGrounded = Physics.Raycast(_groundCheckObj.transform.position,Vector3.down,_maxGroundDist);
-       bool hasJump = Input.GetKeyDown(KeyCode.Space);
-       if (hasJump && _isGrounded)
-       {
-            _rigidbody.AddForce(Vector3.up * 100 * JumpSpeed);
-       }
-       else if (!_isGrounded)
-       {
+        _isGrounded = Physics.Raycast(_groundCheckObj.transform.position, Vector3.down, _maxGroundDist);
+        bool hasJump = Input.GetKeyDown(KeyCode.Space);
+        if (hasJump && _isGrounded)
+        {
+            _rigidbody.AddForce(Vector3.up * (100 * JumpSpeed));
+        }
+        else if (!_isGrounded)
+        {
             _rigidbody.AddForce(Vector3.down * 100);
-       }
+        }
 
         //Вид от 1-го лица.
-        _rotationX -=Input.GetAxis("Mouse Y") * RotationSpeedVer;
-        _rotationX = Mathf.Clamp(_rotationX,MinVer,MaxVer);
+        _rotationX -= Input.GetAxis("Mouse Y") * RotationSpeedVer;
+        _rotationX = Mathf.Clamp(_rotationX, MinVer, MaxVer);
         float delta = Input.GetAxis("Mouse X") * RotationSpeedHor;
-        float _rotationY = transform.localEulerAngles.y + delta;
-        transform.localEulerAngles = new Vector3(_rotationX,_rotationY,0);
+        _rotationY = transform.localEulerAngles.y + delta;
+        
+        Camera.transform.rotation = Quaternion.Euler(_rotationX, _rotationY, 0);
+        transform.rotation = Quaternion.Euler(0, _rotationY, 0);
     }
 }
