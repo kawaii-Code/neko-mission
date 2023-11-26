6ac35752522690f42c32688b5e4b329fd2edb2f3
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-11-26 14:53:07 +0300

    Добавил бесконечную волну врагов и проигрыш

---
+++ Neko-Mission/Assets/Code/InfiniteEnemySpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class InfiniteEnemySpawner : MonoBehaviour
+{
+    public Transform SpawnPoint;
+    public Enemy EnemyPrefab;
+    
+    public float SpawnIntervalMin = 3.0f;
+    public float SpawnIntervalMax = 0.5f;
+
+    public float TimeToGetToMaxDifficultySeconds = 30f;
+
+    private float _timeSinceLastSpawn;
+    private float _currentSpawnInterval;
+    private float _timer;
+
+    private float SpawnInterval =>
+        Mathf.Lerp(SpawnIntervalMin, SpawnIntervalMax, Difficulty);
+    private float Difficulty =>
+        _timer / TimeToGetToMaxDifficultySeconds;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Update()
+    {
+        if (_timeSinceLastSpawn >= _currentSpawnInterval)
+        {
+            Spawn();
+            _timeSinceLastSpawn = 0.0f;
+        }
+        
+        _timeSinceLastSpawn += Time.deltaTime;
+        _timer += Time.deltaTime;
+    }
+
+    private void Spawn()
+    {
+        Instantiate(EnemyPrefab, SpawnPoint.position, Quaternion.identity, transform);
+        _currentSpawnInterval = SpawnInterval;
+    }
+}
---
+++ Neko-Mission/Assets/Code/PlayerBase.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBase : MonoBehaviour
+{
+    public int MaxHealth = 5;
+    public PlayerHealthView View;
+
+    private int _health;
+
+    private void Start()
+    {
+        _health = MaxHealth;
+        View.Show(_health);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            TakeDamage();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void TakeDamage(int damage = 1)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            // Что-то плохое с мышкой
+            SceneManager.LoadScene(0);
+        }
+
+        View.Show(_health);
+    }
+}
