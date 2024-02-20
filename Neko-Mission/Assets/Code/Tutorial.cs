96edae54eefcec9996ed904759d4d513df9799a0
Nia <108984073+kawaii-Code@users.noreply.github.com>
2024-02-20 18:07:09 +0300

    Улучшил туториал 🏫
    - Больше места под текст и картинку за счёт того что я убрал задний фон
    - Добавил прикольную кнопку "Играть!"
    - Текст на слайдах писать удобнее за счёт [TextArea]
    
    Ещё можно сделать картинки на кнопки со стрелочками чтобы вообще круто
    было.

--- Neko-Mission/Assets/Code/Tutorial.cs
+++ Neko-Mission/Assets/Code/Tutorial.cs
@@ -9,9 +9,17 @@ public class Tutorial : MonoBehaviour
     public TMP_Text TutorialComment;
     
     public Sprite[] TutorialSlides;
+    [TextArea]
     public string[] TutorialText;
 
+    public Button PreviousButton;
+    public GameObject NextButton;
+    public GameObject SpecialNextButton;
+
     private int _currentSlideIndex;
+
+    private int SlideCount =>
+        TutorialSlides.Length;
 
     private void Start()
     {
@@ -21,29 +29,43 @@ public class Tutorial : MonoBehaviour
     public void NextSlide()
     {
         Sounds.Play("click3");
-        if (_currentSlideIndex == TutorialSlides.Length - 1)
+        
+        if (_currentSlideIndex == SlideCount - 2)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            NextButton.SetActive(false);
+            SpecialNextButton.SetActive(true);
         }
-        else
-        {
-            _currentSlideIndex++;
-            UpdateSlide();
-        }
+
+        _currentSlideIndex++;
+        UpdateSlide();
     }
 
     public void PreviousSlide()
     {
+        if (_currentSlideIndex == SlideCount - 1)
+        {
+            NextButton.SetActive(true);
+            SpecialNextButton.SetActive(false);
+        }
+        
         if (_currentSlideIndex > 0)
         {
             Sounds.Play("click3");
+
             _currentSlideIndex--;
             UpdateSlide(); 
         }
     }
 
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     private void UpdateSlide()
     {
+        PreviousButton.interactable = _currentSlideIndex != 0;
+
         SlideView.sprite = TutorialSlides[_currentSlideIndex];
         TutorialComment.text = TutorialText[_currentSlideIndex];
     }
