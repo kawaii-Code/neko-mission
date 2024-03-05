1f22704351bad6da6a9281cf926d733c4538fdd1
Nia <108984073+kawaii-Code@users.noreply.github.com>
2024-03-05 19:19:45 +0300

    Приятная мелочь: Прицел убирается при осмотре поля сверху

--- Neko-Mission/Assets/Code/UpCamera.cs
+++ Neko-Mission/Assets/Code/UpCamera.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpCamera : MonoBehaviour
 {
     public Camera MainCamera;
     public Camera UpCAmera;
+    public Image Crosshair;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
+            Crosshair.enabled = !Crosshair.enabled;
             MainCamera.enabled = !MainCamera.enabled;
             UpCAmera.enabled = !UpCAmera.enabled;
             if(UpCAmera.enabled)
